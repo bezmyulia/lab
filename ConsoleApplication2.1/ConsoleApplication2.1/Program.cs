826671b5d96943281c61e2e5a5cd4dfea7ccfefb
bezmyulia <47853889+bezmyulia@users.noreply.github.com>
2020-04-16 19:14:56 +0300

    Add files via upload

--- ConsoleApplication2.1/ConsoleApplication2.1/Program.cs
+++ ConsoleApplication2.1/ConsoleApplication2.1/Program.cs
@@ -61,15 +61,15 @@ namespace ConsoleApplication2._1
             float m = float.Parse(Console.ReadLine()); ;
             float z = (k + m) % 2;
             if (z != 0) Console.WriteLine("2. Белая ячейка");
-            else Console.WriteLine("2. Черная ячейка");
+                else Console.WriteLine("2. Черная ячейка");
 
-            Console.Write("k1=");
+            Console.Write("Введите k1");
             float k1 = float.Parse(Console.ReadLine());
-            Console.Write("m1=");
+            Console.Write("Введите m1");
             float m1 = float.Parse(Console.ReadLine());
-            Console.Write("k2=");
+            Console.Write("Введите k2");
             float k2 = float.Parse(Console.ReadLine());
-            Console.Write("m2=");
+            Console.Write("Введите m2");
             float m2 = float.Parse(Console.ReadLine());
             float x = (k1 + m1) % 2;
             float y = (k2 + m2) % 2;
@@ -78,32 +78,85 @@ namespace ConsoleApplication2._1
             else Console.WriteLine("3. Ячейки одного цвета");
 
             Console.WriteLine("4. Ударит или нет");
-            if ((k1 == k2) && (m2 - m1 == 1)) Console.WriteLine("Пешка ударит");
-            else Console.WriteLine("Пешка не ударит");
-            if ((Math.Abs(k1 - k2)) == Math.Abs((m2 - m1))) Console.WriteLine("Слон ударит");
-            else Console.WriteLine("Слон не ударит");
-            if ((k1 == k2) || (m1 == m2)) Console.WriteLine("Ладья ударит");
-            else Console.WriteLine("Ладья не ударит");
-            if (((Math.Abs(k1 - k2)) == Math.Abs((m2 - m1))) || ((k1 == k2) || (m1 == m2))) Console.WriteLine("Ферзь ударит");
-            else Console.WriteLine("Ферзь не ударит");
-            if (((m2 + 3 == m1) || (m2 - 3 == m1) || (m2 + 1 == m1) || (m2 - 1 == m1)) && ((k2 + 3 == k1) || (k2 - 3 == k1) || (k2 + 1 == k1) || (k2 - 1 == k1))) Console.WriteLine("Конь ударит");
-            else Console.WriteLine("Конь не ударит");
+            if ((k1 == k2) && (m2 - m1 == 1)) Console.WriteLine("Пешка бьет");
+                else Console.WriteLine("Пешка не бьет");
+            if ((Math.Abs(k1 - k2)) == Math.Abs((m2 - m1))) Console.WriteLine("Слон бьет");
+                else Console.WriteLine("Слон не бьет");
+            if ((k1 == k2) || (m1 == m2)) Console.WriteLine("Ладья бьет");
+                else Console.WriteLine("Ладья не бьет");
+            if (((Math.Abs(k1 - k2)) == Math.Abs((m2 - m1))) || ((k1 == k2) || (m1 == m2))) Console.WriteLine("Ферзь бьет");
+                else Console.WriteLine("Ферзь не бьет");
+            if (((m2 - 2) == m1) && ((k2 - 1) == k1) || ((k1 - 1) == k2) || ((m2 + 2) == m1) && ((k2 - 1) == k1) || ((k1 - 1) == k2) || ((k2 - 2) == k1) && ((m2 - 1) == m1) || ((m1 - 1) == m2) || ((k2 + 2) == k1) && ((m2 - 1) == m1) || ((m1 - 1) == m2)) Console.WriteLine("Конь бьет");
+                else Console.WriteLine("Конь не бьет");
 
 
             Console.WriteLine("Задание 2.2");
-            Console.Write("Введите дату в формате 'dd.mm.yyyy:'");
-            DateTime date = DateTime.Parse(Console.ReadLine());
-            DateTime date2 = DateTime.Now;
-            Console.WriteLine("Дней прошло с начала года: "+ date2.Subtract(date));
-            int ny = date2.Year + 1;
-            DateTime nny = new DateTime(ny,1,1);
-            nny = new DateTime(nny.Ticks-1);
-            Console.WriteLine("Дней до конца года: " + nny.Subtract(date));
-            Console.WriteLine("Дата предыдущего дня: " + date.AddDays(-1));
-            Console.WriteLine("Дата следующего дня: " + date.AddDays(+1));
+            int day, month, year, pros = 0, ost = 0, predd = 0, predm = 0, predy = 0, nextd = 0, nextm = 0, nexty = 0;
+            int[] d = { 0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 };
+            Console.WriteLine("Введите день ");
+            day = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите месяц ");
+            month = int.Parse(Console.ReadLine());
+            Console.WriteLine("Введите год ");
+            year = int.Parse(Console.ReadLine());
+            int m = month;
+            while (m > 0)
+            { pros += d[m]; m--; }
+            m = month;
+            while (m < 13)
+            { ost += d[m]; m++; }
+            if (day == 1)
+            {
+                if (month == 1)
+                {
+                    predm = 12;
+                    predy = year - 1;
+                    predd = d[predm];
+                }
+                else
+                {
+                    predd = d[month - 1];
+                    predm = month - 1;
+                    predy = year;
+                }
+            }
+            else
+            {
+                predd = day - 1;
+                predm = month;
+                predy = year;
+            }
+            if (day == d[month])
+            {
+                if (month == 12)
+                {
+                    nextm = 1;
+                    nexty = year + 1;
+                    nextd = 1;
+                }
+                else
+                {
+                    nextd = 1;
+                    nextm = month + 1;
+                    nexty = year;
+                }
+            }
+            else
+            {
+                nextd = day + 1;
+                nextm = month;
+                nexty = year;
+            }
+            pros -= d[month] - day;
+            ost -= day;
+            Console.WriteLine("С начала года прошло {0} дней", pros);
+            Console.WriteLine("До конца года осталось {0} дней", ost);
+            Console.WriteLine("Вчерашняя дата - {0}:{1}:{2}", predd, predm, predy);
+            Console.WriteLine("Завтрашняя дата - {0}:{1}:{2}", nextd, nextm, nexty); 
+            */
 
-            Console.WriteLine("Задание 2.3");
-            Console.WriteLine("Числа Армстронга: ");
+             Console.WriteLine("Задание 2.3");
+             Console.WriteLine("Числа Армстронга: ");
                  for (int i = 100; i < 999; i++)
             {
                     int a1 = i / 100;
@@ -111,7 +164,7 @@ namespace ConsoleApplication2._1
                     int a3 = i % 10;
                 if (i == Math.Pow(a1, 3) + Math.Pow(a2, 3) + Math.Pow(a3, 3))
                     Console.WriteLine(i); 
-        }*/
+        }
 
             Console.WriteLine("Задание 2.4");
             Console.Write("Введите сторону n:");
@@ -123,18 +176,14 @@ namespace ConsoleApplication2._1
             Console.WriteLine("Квадратов наименьшей площади: "+ mal); 
             while (n != m)
             {
-                if (n > m) {
-                    n = n - m;
-                    k++;
-                }
-                else {
-                        m = m - n;
-                        k++;
-                }   
+                if (n > m) { n = n - m; k++; }
+                else { m = m - n; k++; }   
             }
             k++;
             Console.WriteLine("Квадратов набольшей площади: "+ k);
             
+
+
             /*
                         Console.WriteLine("Задание 1");
                         Console.Write("Сторона а=");
