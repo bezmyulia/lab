ed6a740ca37618be31c5b70fad97e3d389122ed0
bezmyulia <47853889+bezmyulia@users.noreply.github.com>
2020-05-26 13:24:23 +0300

    Add files via upload

---
+++ console application 6/6.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Laborr6
+{
+    class Program
+    {
+        public static void st(double a, out int sign, out bool isInteger, out double abs, out double sqr)
+        {
+            sign = Math.Sign(a);
+            isInteger = a % 1 == 0 ? true : false;
+            abs = Math.Abs(a);
+            sqr = a * a;
+        }
+        static void Main()
+        {
+            double a = 42;
+            double b = -12.67;
+
+            double c, d;
+            int i;
+            bool tf;
+
+            st(a, out i, out tf, out c, out d);
+            Console.WriteLine($"Число: {a}\nЗнак числа {i}; Целое: {tf}, Модуль числа: {c}, Квадрат числа: {d}");
+            st(b, out i, out tf, out c, out d);
+            Console.WriteLine($"Число: {b}\nЗнак числа {i}; Целое: {tf}, Модуль числа: {c}, Квадрат числа: {d}");
+        }
+    }
+}
