b49a21210a928f3b472c6b941208917c3c9a4596
bezmyulia <47853889+bezmyulia@users.noreply.github.com>
2020-05-26 13:26:51 +0300

    Add files via upload

---
+++ console application 6/6.4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Laborr6
+{
+    class Program
+    {
+        public static double Avarge(params int[] arr)
+
+        {
+            double av = 0;
+            foreach (int elem in arr)
+                av += elem;
+            return (av / arr.Length);
+        }
+        static void Main()
+        {
+            int[] a = new int[] { 1, 2, 3, 4 };
+            Console.WriteLine(Avarge(a));
+
+            short sh1 = 1, sh2 = 12;
+            byte bt = 107;
+            Console.WriteLine(Avarge(sh1, sh2, bt));
+        }
+    }
+}
