92811697baf0bbad541bd5f6e45574d114f64e9a
bezmyulia <47853889+bezmyulia@users.noreply.github.com>
2020-05-27 20:56:17 +0300

    Add files via upload

---
+++ console application 8/List-Value-Types/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace List_Value_Types
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SByte a = 127;
+            Byte b = 255;
+            Int16 i1 = 16;
+            Int32 i2 = 32;
+            Int64 i3 = 64;
+            String s = "";
+            Exception ex = new Exception();
+            Object[] obj = { a, b, i1, i2, i3, s, ex };
+
+            foreach (var a1 in obj)
+            {
+                Console.WriteLine($"{a1.GetType()}:\t{a1.GetType().IsValueType}");
+            }
+        }
+    }
+}
