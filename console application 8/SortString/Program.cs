f6580c58f651143270ae9fd517e7802341d63556
bezmyulia <47853889+bezmyulia@users.noreply.github.com>
2020-05-27 20:57:21 +0300

    Add files via upload

---
+++ console application 8/SortString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Linq;
+
+namespace SortString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "Лабораторная работа №8";
+            Console.WriteLine(s);
+            string[] sa = s.Split(' ');
+
+            Array.Sort(sa);
+
+            string saa = string.Join(" ", sa);
+            Console.WriteLine(saa);
+        }
+    }
+}
